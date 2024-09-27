6d8b6873d5fca25f8f0044887184aeb2e389239c
Harika Madhavapeddy <harikamadhavapeddy91@gmail.com>
2024-09-27 11:23:10 -0500

    firebase commit

--- src/Firebase/Firebase.js
+++ src/Firebase/Firebase.js
@@ -8,15 +8,7 @@ import { getDatabase } from "firebase/database";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  /*apiKey: process.env.PRIDE_API_KEY,
-  authDomain: process.env.PRIDE_AUTH_DOMAIN,
-  projectId: process.env.PRIDE_PROJECT_ID,
-  storageBucket: process.env.PRIDE_STORAGE_BUCKET,
-  databaseURL: process.env.PRIDE_DATABASE_URL,
-  messagingSenderId: process.env.PRIDE_MESSAGE_SENDER,
-  appId: process.env.PRIDE_APP_ID,
-  measurementId: process.env.PRIDE_MEASUREMENT_ID*/
-  apiKey: "AIzaSyDvOo_j3zUWQmNYPzcSsrxrV5uDY8iflSA",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
   databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
@@ -26,8 +18,6 @@ const firebaseConfig = {
   measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
-// Initialize Firebase
-console.log('harika');
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const database=getDatabase(app);
